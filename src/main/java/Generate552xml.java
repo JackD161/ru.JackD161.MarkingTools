469757e229c2596c48eaa8a78aa3dbe334b4e85c
Kholopkin JackD161 Yuriy <holopkin_yurik@mail.ru>
2024-02-20 23:33:45 +0300

    Переделаны классы генерации документов по схемам 552, 701 на новую систему чтения файла данных об SGTINах

--- src/main/java/Generate552xml.java
+++ src/main/java/Generate552xml.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 // класс генерирует документ xml для вывода из оборота товара отправителем
 public class Generate552xml {
